613c3cc5163dc4638d4dc6f2734f9116d98e3a50
Roger Nora <109190689+RogerNora@users.noreply.github.com>
2025-07-30 19:37:34 -0300

    Finalizado

--- script.js
+++ script.js
@@ -11,9 +11,18 @@ let lin = prompt("escolha um numero de 0 a 3?");
 
 
 let linguagens = ["java", "python", "cobol", "assemble"];
+console.log(linguagens);
 
 let muda = document.querySelector("#lista");
  muda.textContent = linguagens[0] + " - " + linguagens[1] + " - " + linguagens[2]+ " - " + linguagens[3];
 
 
+let aluno1 = {
+    nome: "Roger",
+    idade: 39,
+    anoLetivo: "5ªsérie",
+    materiasFavoritas: ["Física", 'Programação', "Robótica"]
+}
 
+const objeto = document.querySelector("#aluno");
+objeto.textContent = aluno1.materiasFavoritas;
